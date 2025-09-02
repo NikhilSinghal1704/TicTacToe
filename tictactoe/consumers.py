4fb8c170bfb09688efed51002b6eb36bfb352561
Nikhil Singhal <nikhilsinghal1704@gmail.com>
2025-09-03 01:46:20 +0530

    websocket v0.3

--- tictactoe/consumers.py
+++ tictactoe/consumers.py
@@ -4,6 +4,11 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from .models import Room, Game
 
+import json, random
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Room, Game
+
 class TicTacToeConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_code = self.scope["url_route"]["kwargs"]["room_code"]
@@ -18,8 +23,6 @@ class TicTacToeConsumer(AsyncWebsocketConsumer):
         room = await self.get_room(self.room_code)
         if room:
             await self.add_player(room, self.player_id)
-
-            # Broadcast updated room status (waiting / players)
             await self.broadcast_room_state(room)
 
     async def disconnect(self, close_code):
@@ -27,7 +30,6 @@ class TicTacToeConsumer(AsyncWebsocketConsumer):
         if room:
             await self.remove_player(room, self.player_id)
             await self.broadcast_room_state(room)
-
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
@@ -42,14 +44,22 @@ class TicTacToeConsumer(AsyncWebsocketConsumer):
             await self.send_game_history()
 
     # -----------------------------
-    # Room & Game logic
+    # Game logic
     # -----------------------------
     async def handle_start_game(self):
-        """Only start if 2 players are connected."""
+        """Only host can start and require 2 players."""
         room = await self.get_room(self.room_code)
-        if room and len(room.players) == 2:
-            game = await self.create_new_game(room)
-            await self.broadcast_game_started(game)
+        if not room or len(room.players) < 2:
+            await self.send(json.dumps({"error": "Need 2 players to start"}))
+            return
+
+        # Only host can start
+        if self.player_id != room.host_id:
+            await self.send(json.dumps({"error": "Only host can start game"}))
+            return
+
+        game = await self.create_new_game(room)
+        await self.broadcast_game_started(game)
 
     async def handle_make_move(self, data):
         index = data.get("index")
@@ -60,33 +70,28 @@ class TicTacToeConsumer(AsyncWebsocketConsumer):
             await self.send(json.dumps({"error": "No active game"}))
             return
 
+        # Determine current turn
+        turn = self.current_turn(game)
+        if (player_id == game.x_player and turn != "X") or (player_id == game.o_player and turn != "O"):
+            await self.send(json.dumps({"error": "Not your turn"}))
+            return
+
         # Validate move
         if game.finished or index < 0 or index > 8 or game.board[index] != "":
             await self.send(json.dumps({"error": "Invalid move"}))
             return
 
-        symbol = "X" if player_id == game.x_player else "O" if player_id == game.o_player else None
-        if not symbol:
-            await self.send(json.dumps({"error": "Not a participant"}))
-            return
-
+        symbol = "X" if player_id == game.x_player else "O"
         game.board[index] = symbol
+
+        # Check winner
         winner = self.check_winner(game.board)
         if winner:
             game.finished = True
             game.winner = winner
-        await database_sync_to_async(game.save)()
 
-        # Broadcast updated board
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "game_update",
-                "board": game.board,
-                "finished": game.finished,
-                "winner": game.winner
-            }
-        )
+        await database_sync_to_async(game.save)()
+        await self.broadcast_game_update(game)
 
     # -----------------------------
     # Broadcast helpers
@@ -94,11 +99,7 @@ class TicTacToeConsumer(AsyncWebsocketConsumer):
     async def broadcast_room_state(self, room):
         await self.channel_layer.group_send(
             self.room_group_name,
-            {
-                "type": "room_update",
-                "players": room.players,
-                "host": room.host_id
-            }
+            {"type": "room_update", "players": room.players, "host": room.host_id}
         )
 
     async def broadcast_game_started(self, game):
@@ -108,23 +109,33 @@ class TicTacToeConsumer(AsyncWebsocketConsumer):
                 "type": "game_started",
                 "x_player": game.x_player,
                 "o_player": game.o_player,
-                "board": game.board
+                "board": game.board,
+                "turn": "X"  # X always starts
+            }
+        )
+
+    async def broadcast_game_update(self, game):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "game_update",
+                "board": game.board,
+                "finished": game.finished,
+                "winner": game.winner,
+                "x_player": game.x_player,
+                "o_player": game.o_player,
+                "turn": self.current_turn(game)
             }
         )
 
     async def send_game_history(self):
-        """Send all previous games in this room."""
         room = await self.get_room(self.room_code)
         if room:
-            history = []
-            for game in room.games.all():
-                history.append({
-                    "x_player": game.x_player,
-                    "o_player": game.o_player,
-                    "board": game.board,
-                    "finished": game.finished,
-                    "winner": game.winner
-                })
+            history = [
+                {"x_player": g.x_player, "o_player": g.o_player, "board": g.board,
+                 "finished": g.finished, "winner": g.winner}
+                for g in room.games.all()
+            ]
             await self.send(json.dumps({"history": history}))
 
     # -----------------------------
@@ -140,19 +151,19 @@ class TicTacToeConsumer(AsyncWebsocketConsumer):
         await self.send(json.dumps(event))
 
     # -----------------------------
-    # Winner check
+    # Utility methods
     # -----------------------------
     def check_winner(self, board):
-        winning_combos = [
-            [0,1,2],[3,4,5],[6,7,8],
-            [0,3,6],[1,4,7],[2,5,8],
-            [0,4,8],[2,4,6]
-        ]
-        for combo in winning_combos:
-            a,b,c = combo
-            if board[a] and board[a] == board[b] == board[c]:
+        combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        for a,b,c in combos:
+            if board[a] and board[a]==board[b]==board[c]:
                 return board[a]
         return None
+
+    def current_turn(self, game):
+        """Return whose turn it is: X on even moves, O on odd."""
+        moves_played = len([s for s in game.board if s])
+        return "X" if moves_played % 2 == 0 else "O"
 
     # -----------------------------
     # Database operations
@@ -174,19 +185,13 @@ class TicTacToeConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def create_new_game(self, room):
-        """Create a new Game for this room and assign X/O randomly."""
         players = room.players.copy()
         random.shuffle(players)
-        game = Game.objects.create(
-            room=room,
-            x_player=players[0],
-            o_player=players[1],
-            board=[""]*9
-        )
-        return game
+        return Game.objects.create(room=room, x_player=players[0], o_player=players[1], board=[""]*9)
 
     @database_sync_to_async
     def get_active_game(self, room_code):
         room = Room.objects.get(code=room_code)
         games = room.games.filter(finished=False)
         return games.first() if games.exists() else None
+
